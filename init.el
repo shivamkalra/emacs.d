a38d05470d4f2b70e1f09dafe1dac3d0b9bb01e4
Shivam Kalra <skalra@klick.com>
2015-03-04 01:48:28 -0500

    * init.el:

--- init.el
+++ init.el
@@ -11,3 +11,17 @@
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
+;; TODO: move to custom lisp 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(yas-prompt-functions (quote (my-yas/prompt))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-selection ((t (:background "#002B36" :underline t))))
+ '(helm-source-header ((t (:background "#002B36" :foreground "#2AA198" :underline nil)))))
