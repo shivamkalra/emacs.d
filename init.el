4959cfbd5c3bbb8ea8a88970b0eadee43057c7f3
Shivam Kalra <skalra@klick.com>
2015-03-16 18:35:01 -0400

    * All: things broken on updates

--- init.el
+++ init.el
@@ -11,3 +11,19 @@
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-set-upstream-on-push (quote dontask))
+ '(magit-use-overlays nil)
+ '(yas-prompt-functions (quote (my-yas/prompt))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "gold" :foreground "#002b36" :inverse-video t))))
+ '(fringe ((t (:background "#003036" :foreground "#586e75"))))
+ '(hl-line ((t (:background "#1e3337")))))
