77b548d2d6f3e6e9e4f40f7733679e523635c68a
Shivam Kalra <skalra@klick.com>
2015-03-17 18:20:01 -0400

    * TODO: Added Slime still need integration swank-js

--- init.el
+++ init.el
@@ -16,14 +16,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-set-upstream-on-push (quote dontask))
- '(magit-use-overlays nil)
  '(yas-prompt-functions (quote (my-yas/prompt))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "gold" :foreground "#002b36" :inverse-video t))))
- '(fringe ((t (:background "#003036" :foreground "#586e75"))))
- '(hl-line ((t (:background "#1e3337")))))
+ '(cursor ((t (:background "yellow" :foreground "#002b36" :inverse-video t))))
+ '(hl-line ((t (:background "#1b2a2e")))))
