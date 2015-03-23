c86008afb75ed382dc8dfdeebdfd213003a7de7c
Shivam Kalra <skalra@klick.com>
2015-03-23 10:08:57 -0400

    * init.el: Removing code added by customization (already included)

--- init.el
+++ init.el
@@ -11,15 +11,3 @@
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(yas-prompt-functions (quote (my-yas/prompt))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "yellow" :foreground "#002b36" :inverse-video t)))))
